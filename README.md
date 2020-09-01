4ec725ee95acb710246daa603aec82d74c898da0
alexstar55 <zhangzhiming101@126.com>
2020-09-01 09:26:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Practice/Assignments of GAMES101  
+ipynb files can run C++ using Xeus-Cling.
---
+++ work0.cpp
@@ -0,0 +1,11 @@
+#include <cmath >
+#include <iostream >
+int main (){
+    float a = 1.0 , b = 2.0;
+    std :: cout << a <<std :: endl;
+    std :: cout << a/b <<std :: endl;
+    std :: cout << std :: sqrt(a) <<std :: endl;
+    std :: cout << std :: acos (-1) << std :: endl;
+    std :: cout << std :: sin (30.0/180.0* acos ( -1)) << std :: endl;
+    return 0;
+}
